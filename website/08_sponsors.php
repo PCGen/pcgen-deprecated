6ea5217aa42b1bd1495b8fc6525a72accda6f1ff
karianna <karianna@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-05-20 14:16:45 +0000

    Added sponsors page
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@1003 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ website/08_sponsors.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('include/header.php.inc');
+
+?>
+
+    <div id="content" class="content">
+
+        <h1>Official Sponsor</h1>
+        Recently PCGen has opened up its community effort to sponsorship, we'd like to acknowledge the 
+		following sponsor who help us keep PCGen going for the role-playing community at large!<br />
+		<br />
+        <p align="center">
+			<a href="http://www.silvenpublishing.com"><img src="images/silven_logo_small.gif" /></a>
+	    </p>
+
+        <h1>Friends of PCGen</h1>
+		PCGen has always had many supporters and there are a couple we'd like to thank for 
+		their moral support and contributions to the project!
+		<p>
+			<ul>
+				<li><a href="http://www.codemonkeypublishing.com">Code Monkey Publishing</a></li>
+				<li><a href="http://www.battlefieldpress.com">Battlefield Press</a></li>
+			</ul>
+		</p>
+    </div> <!-- div content -->
+
+<?php
+
+require_once('include/footer.php.inc');
+
+?>
