6c82c60d167ba9f0b9259e88e4cbf594c274c21d
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-05-26 21:54:47 +0000

    Point to new autobuild pages.
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@1038 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- website/07_autobuilds.php
+++ website/07_autobuilds.php
@@ -7,20 +7,22 @@ require_once('include/header.php.inc');
     <div id="content" class="content">
 
         <h1>PCGen Autobuild site</h1>
-        <h2>PCGen Autobuilds</h2>
+        <h2>PCGen Development Autobuilds</h2>
+        <p>These are regular scheduled builds from the development branch of PCGen. Every four hours, 
+        the project is checked for changes and if any have been made a new build is generated.</p>
         <ul>
-            <li><a href="http://www.legolas.org/pcgen/autobuilds/">PCGen Autobuilds (Built every 4 hours)</a></li>
+          <li><a href="http://pcgen.sourceforge.net/autobuilds/">Autobuild report</a>
+          <li><a href="https://sourceforge.net/project/showfiles.php?group_id=25576&package_id=191558&release_id=419203">Download Autobuild</a>
         </ul>
-				
-        <h2>PCGen Autobuild Documentation</h2>
+
+        <h2>PCGen Production Autobuilds</h2>
+        <p>These are regular scheduled builds from the production branch of PCGen. Every four hours, 
+        the project is checked for changes and if any have been made a new build is generated.
+        This branch is only active when there is work going on to release a production fix release, 
+        or when we are nearing a new full production release.</p>
         <ul>
-            <li><a href="http://www.legolas.org/pcgen/docs/">PCGen Docs (Built every 4 hours)</a></li>
-            <li><a href="http://www.legolas.org/pcgen/autobuilds/doc/doc/api/">JavaDocs (Built every 4 hours)</a></li>
-<!--            
-            <li><a href="http://www.legolas.org/pcgen/statcvs/">CVS Stats (Built Daily)</a></li>
-            <li><a href="http://www.legolas.org/pcgen/test-results/">jUnit test results (Built Daily)</a></li>
-            <li><a href="http://www.legolas.org/pcgen/coverage/">jUnit Code Coverage (Build Daily)</a></li>
--->            
+          <li><a href="http://pcgen.sourceforge.net/autobuilds-prod/">Autobuild report</a>
+          <li><a href="https://sourceforge.net/project/showfiles.php?group_id=25576&package_id=191558&release_id=419532">Download Autobuild</a>
         </ul>
 
     </div> <!-- div content -->
