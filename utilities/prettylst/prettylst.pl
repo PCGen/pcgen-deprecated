eb78f7a9ebeeef2e05160fc75dfc71b5e9bedb6f
tir-gwaith <tir-gwaith@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-03-01 10:30:38 +0000

    adding some GameMode defined VARs
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2278 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- utilities/prettylst/prettylst.pl
+++ utilities/prettylst/prettylst.pl
@@ -527,6 +527,8 @@ my @valid_system_var_names      = qw(
     VEHICLEHANDLING         VEHICLEHARDNESS             VEHICLESPEED
     VEHICLETOPMPH           VEHICLEWOUNDPOINTS          Wealth
     CR                      CL                          ECL
+    SynergyBonus            NoTypeProficiencies         NormalMount
+                                                        NormalFollower
 
     Action                      ActionLVL                   ArmorQui
     ClassDefense                DamageThreshold             DenseMuscle
