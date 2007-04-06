e4a2e5ac1f14435522edcd8615c2d4f9ecbd0f58
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-04-06 20:32:29 +0000

    CDOM: Re-add WeaponProf Token Tests (in src/utest)
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/cdom@2678 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/weaponprof/TokenWeaponProfTestSuite.java
@@ -0,0 +1,13 @@
+package plugin.lsttokens.weaponprof;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import junit.framework.TestSuite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({HandsTokenTest.class})
+public class TokenWeaponProfTestSuite extends TestSuite
+{
+	//No content, see annotations
+}
