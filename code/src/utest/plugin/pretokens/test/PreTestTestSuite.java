9dcb339c353ce4fbf1905629bd75239529243d4a
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-09-30 21:26:25 +0000

    Commit unit test compilation
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/cdom@4198 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/pretokens/test/PreTestTestSuite.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.pretokens.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import junit.framework.TestSuite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PreAbilityTesterTest.class, PreAlignTesterTest.class,
+	PreBirthPlaceTesterTest.class, PreCityTesterTest.class,
+	PreCSkillTesterTest.class, PreDamageReductionTesterTest.class,
+	PreDeityAlignTesterTest.class, PreDeityDomainTesterTest.class,
+	PreDeityTesterTest.class, PreDomainTesterTest.class,
+	PreEquipBothTesterTest.class, PreEquipPrimaryTesterTest.class,
+	PreEquipSecondaryTesterTest.class, PreEquipTesterTest.class,
+	PreEquipTwoWeaponTesterTest.class, PreFeatTesterTest.class,
+	PreGenderTesterTest.class, PreHandsTesterTest.class,
+	PreHasDeityTesterTest.class, PreLanguageTesterTest.class,
+	PreLegsTesterTest.class, PreRaceTesterTest.class, PreReachTesterTest.class,
+	PreRegionTesterTest.class, PreShieldProfTesterTest.class,
+	PreSizeTesterTest.class, PreSkillMultTesterTest.class,
+	PreSkillTesterTest.class, PreSkillTotTesterTest.class,
+	PreSpecialAbilityTesterTest.class, PreSpellBookTesterTest.class,
+	PreSpellCastMemorizeTesterTest.class, PreSpellCastTesterTest.class,
+	PreSpellDescriptorTesterTest.class, PreSpellResistanceTesterTest.class,
+	PreSpellSchoolTesterTest.class, PreSpellSubSchoolTesterTest.class,
+	PreSpellTesterTest.class, PreStatTesterTest.class,
+	PreSubClassTesterTest.class, PreTemplateTesterTest.class,
+	PreTextTesterTest.class, PreTypeTesterTest.class,
+	PreVisionTesterTest.class, PreWeaponProfTesterTest.class,
+	PreWieldTesterTest.class,})
+public class PreTestTestSuite extends TestSuite
+{
+	// No content, see annotations
+}
