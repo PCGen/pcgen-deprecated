22438678c41cd9797d564ecdc622bd1c05f1d0c6
karianna <karianna@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-05-08 14:26:51 +0000

    Added further unit tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/cdom@6219 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/utest/pcgen/base/graph/command/IllegalGraphStateExceptionTest.java
+++ code/src/utest/pcgen/base/graph/command/IllegalGraphStateExceptionTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class IllegalGraphStateExceptionTest extends TestCase {
 
 	@Test
-	public void testEmptyConstructor()
+	public void testConstructor()
 	{
 		String expectedResult = "Message"; 
 		IllegalGraphStateException illegalGraphStateException = new IllegalGraphStateException("Message");
--- code/src/utest/pcgen/base/graph/monitor/GraphMismatchExceptionTest.java
+++ code/src/utest/pcgen/base/graph/monitor/GraphMismatchExceptionTest.java
@@ -1,5 +1,26 @@
 package pcgen.base.graph.monitor;
 
-public class GraphMismatchExceptionTest {
+import junit.framework.TestCase;
 
+import org.junit.Test;
+
+public class GraphMismatchExceptionTest extends TestCase {
+
+	@Test
+	public void testConstructor()
+	{
+		String expectedResult = "Message"; 
+		GraphMismatchException graphMismatchException = new GraphMismatchException("Message");
+		assertNotNull(graphMismatchException);
+		String result = graphMismatchException.getMessage();
+		assertTrue(result.equals(expectedResult));
+	}
+
+	@Test
+	public void testEmptyConstructor()
+	{
+		GraphMismatchException graphMismatchException = new GraphMismatchException();
+		assertNotNull(graphMismatchException);
+	}
+	
 }
