0c1f9e73c65c534c493b47572914903226c1fd37
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-06-08 08:06:21 +0000

    Update links for 5.14.0
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@6500 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- website/03_get_pcgen.php
+++ website/03_get_pcgen.php
@@ -115,9 +115,9 @@ function writePcgenFileItems($url, $viewProd, $maxRecs)
         <a href="http://sourceforge.net/project/showfiles.php?group_id=25576&package_id=129606" style="font-size: 80%;">[View Older Production Releases]</a>
         </p>
         
-        <div class="downloadbar"><a href="http://downloads.sourceforge.net/pcgen/pcgen5121_win_install.exe">Download PCGen 5.12.1 for Windows<small>&nbsp;</small></a></div><br />
-        <div class="downloadbar"><a href="http://downloads.sourceforge.net/pcgen/pcgen5121_mac_install.dmg">Download PCGen 5.12.1 for Mac<small>&nbsp;</small></a></div><br />
-        <div class="downloadbar"><a href="http://downloads.sourceforge.net/pcgen/pcgen5121_full.zip">Download PCGen 5.12.1 for Other Systems<small>&nbsp;</small></a></div><br/>
+        <div class="downloadbar"><a href="http://downloads.sourceforge.net/pcgen/pcgen5140_win_install.exe">Download PCGen 5.14.0 for Windows<small>&nbsp;</small></a></div><br />
+        <div class="downloadbar"><a href="http://downloads.sourceforge.net/pcgen/pcgen5140_mac_install.dmg">Download PCGen 5.14.0 for Mac<small>&nbsp;</small></a></div><br />
+        <div class="downloadbar"><a href="http://downloads.sourceforge.net/pcgen/pcgen5140_full.zip">Download PCGen 5.14.0 for Other Systems<small>&nbsp;</small></a></div><br/>
 
         <h2>Option 2. Download Alpha Releases<a class="" title="alpha" name="alpha"></a></h2>
         <p>These are development milestone releases designed to display the work in progress on PCGen
