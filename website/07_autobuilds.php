964fc20b713408fb4c800a5245015b909f154473
maredudd800 <maredudd800@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-06-16 05:07:24 +0000

    1992957, Webpage out of date. Updated the DEv and Stable version numbers.
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/5.14.x@6687 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- website/07_autobuilds.php
+++ website/07_autobuilds.php
@@ -13,7 +13,7 @@ require_once('include/header.php.inc');
         it is best to use one of our regular releases available from the 
         <a href="http://pcgen.sourceforge.net/03_get_pcgen.php">downloads page</a> .</p>
         
-        <h2>PCGen Development Autobuilds (5.13.x)</h2>
+        <h2>PCGen Development Autobuilds (5.15.x)</h2>
         <p>These are regular scheduled builds from the development branch of PCGen. Every night, 
         the project is checked for changes and if any have been made a new build is generated.</p>
         <ul>
@@ -21,12 +21,12 @@ require_once('include/header.php.inc');
           <li><a href="http://pcgen.sourceforge.net/autobuilds/download.html">Download Autobuild</a>
         </ul>
 
-        <h2>PCGen Production Autobuilds (5.12.x)</h2>
+        <h2>PCGen Production Autobuilds (5.14.x)</h2>
         <p>These are regular scheduled builds from the production branch of PCGen. Every night, 
         the project is checked for changes and if any have been made a new build is generated.
         This branch is only active when there is work going on to release a production fix release, 
         or when we are nearing a new full production release. The builds currently show any fixes we 
-        are doing to the 5.12.0 release.</p>
+        are doing to the 5.14.0 release.</p>
         <ul>
           <li><a href="http://pcgen.sourceforge.net/autobuilds-prod/">Autobuild report</a>
           <li><a href="http://pcgen.sourceforge.net/autobuilds-prod/download.html">Download Autobuild</a>
