94f16756190c00e1990edb6855848b831eca606a
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-07-05 04:43:05 +0000

    Fix bug: NPE with loading companion Issue#: 2009246
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/5.14.x@7010 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- pcgen/code/src/java/pcgen/core/kit/KitStat.java
+++ pcgen/code/src/java/pcgen/core/kit/KitStat.java
@@ -109,6 +109,11 @@ public class KitStat extends BaseKit implements Serializable, Cloneable
 
 	public void apply(PlayerCharacter aPC)
 	{
+		// Non abilities will be null, so skip those.
+		if (theStat == null)
+		{
+			return;
+		}
 		final StatList aStatList = aPC.getStatList();
 		for (Iterator<PCStat> stat = aStatList.iterator(); stat.hasNext();)
 		{
