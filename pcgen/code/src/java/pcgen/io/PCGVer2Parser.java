9b9b79c181caeafad62688de2f17f2551f465bc3
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-10-30 22:56:00 +0000

    problem reloading Mystic Theurge Issue#: 2886125
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/5.16.x@10849 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- pcgen/code/src/java/pcgen/io/PCGVer2Parser.java
+++ pcgen/code/src/java/pcgen/io/PCGVer2Parser.java
@@ -1872,13 +1872,19 @@ final class PCGVer2Parser implements PCGParser, IOConstants
 				+ ")\nCould not find any ADD: " + name + "|" + dString);
 			return;
 		}
+		boolean found = false;
 		for (PersistentTransitionChoice<?> tc : addList)
 		{
-			processTransitionChoice(cdo, it2, name, dString, tc);
+			found |= processTransitionChoice(cdo, it2, name, dString, tc);
+		}
+		if (!found)
+		{
+			warnings.add(cdo.getDisplayName() + "(" + cdo.getClass().getName()
+					+ ")\nCould not find matching ADD: " + name + "|" + dString);
 		}
 	}
 
-	private <T> void processTransitionChoice(CDOMObject cdo,
+	private <T> boolean processTransitionChoice(CDOMObject cdo,
 		Iterator<PCGElement> it2, String name, String dString,
 		PersistentTransitionChoice<T> tc)
 	{
@@ -1902,11 +1908,11 @@ final class PCGVer2Parser implements PCGParser, IOConstants
 					tc.restoreChoice(thePC, cdo, tc.castChoice(obj));
 				}
 			}
+			return true;
 		}
 		else
 		{
-			warnings.add(cdo.getDisplayName() + "(" + cdo.getClass().getName()
-				+ ")\nCould not find matching ADD: " + name + "|" + dString);
+			return false;
 		}
 	}
 
