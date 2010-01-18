ed9e7094003c3a47202e8c38e3df64664d5f1600
karianna <karianna@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-01-18 11:48:11 +0000

    Updated instructions on bug and freq submission
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@11319 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- website/06_trackers.php
+++ website/06_trackers.php
@@ -44,18 +44,9 @@ require_once('include/header.php.inc');
         </ul>
 
         <h2>Submitting A New Tracker</h2>
-
-        <p>This is an example demonstrating the addition of a Feature Request. Bug Reports and Support Requests work the same way.</p>
-        <p>Go to the PCGen SourceForge home page: &lt;<a href="http://pcgen.sourceforge.net/">http://pcgen.sourceforge.net/</a>&gt;.</p>
-        <p>Click &quot;Request Feature&quot; in the menu bar (below the title, above the tab bar).</p>
-        <p>If you are not logged in to SourceForce yet, log in. There is a link on the left entitled &quot;Login  via SSL&quot;. (After you log in, you may need to click this link again: &lt;<a href="http://pcgen.sourceforge.net/">http://pcgen.sourceforge.net/</a>&gt;.)</p>
-        <p>On the Feature Request page, click &quot;Submit New&quot;.</p>
-        <p>Select the appropriate &quot;Category&quot; and &quot;Group&quot; for your request. &quot;Category&quot; is the general subject of your feature request. &quot;Group&quot; is the version of the PCGen that you are requesting be modified.</p>
-        <p>Fill in a meaningful &quot;Summary&quot; (such that the Summary makes it obvious at a glance what feature you are requesting), and then give the whole scoop in the &quot;Detailed Description&quot;.</p>
-        <p>&quot;Submit&quot;, and you are done.</p>
+        <p>There are some <a href="http://pcgen.sourceforge.net/autobuilds/pcgen-docs/faqpages/faqsubmittingabugreport.html">Bug Submission Guidelines</a> and <a href="http://pcgen.sourceforge.net/autobuilds/pcgen-docs/faqpages/faqsubmittingafeaturerequest.html">Feature Request Submission Guidelines</a> to follow when submitting an issue.</p>
 
         <h2>Monitoring Issue Trackers</h2>
-
         <p>You may view a list of all the issue trackers that you have submitted by clicking the &quot;my sf.net&quot; button at the top of the SourceForge page.</p>
 
     </div> <!-- div content -->
