8370ab397d53f839c8e23045caac5e4ab93d3213
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-06-30 11:25:10 +0000

    Fix bug: Template Editor: Errors in console on save
    Issue#: CODE-145
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/5.16.x@12388 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- pcgen/code/src/java/pcgen/gui/editor/EditorMainForm.java
+++ pcgen/code/src/java/pcgen/gui/editor/EditorMainForm.java
@@ -875,9 +875,12 @@ public final class EditorMainForm extends JDialog
 
 				PCTemplate thisPCTemplate = (PCTemplate) thisPObject;
 				thisPCTemplate.removeListFor(ListKey.MOVEMENT);
-				Movement cmv = Movement.getMovementFrom(pnlMovement.getMoveValues());
-				cmv.setMoveRatesFlag(pnlMovement.getMoveRateType());
-				thisPCTemplate.addToListFor(ListKey.MOVEMENT, cmv);
+				if (pnlMovement.getMoveValues().length() > 0)
+				{
+					Movement cmv = Movement.getMovementFrom(pnlMovement.getMoveValues());
+					cmv.setMoveRatesFlag(pnlMovement.getMoveRateType());
+					thisPCTemplate.addToListFor(ListKey.MOVEMENT, cmv);
+				}
 
 				thisPCTemplate.removeAllFromList(Vision.VISIONLIST);
 				List<Vision> tplVisionList = pnlVision.getVision();
@@ -917,32 +920,44 @@ public final class EditorMainForm extends JDialog
 				// Save favoured classes
 				//
 				sel = pnlClasses.getSelectedList();
-				aString = EditUtil.delimitArray(sel, '|');
-				context.unconditionallyProcess(thisPCTemplate, "FAVOREDCLASS", aString);
+				if (sel.length > 0)
+				{
+					aString = EditUtil.delimitArray(sel, '|');
+					context.unconditionallyProcess(thisPCTemplate, "FAVOREDCLASS", aString);
+				}
 
 				//
 				// Save choice of auto languages
 				//
 				sel = pnlLanguages.getSelectedList2();
-				aString = EditUtil.delimitArray(sel, '|');
-				context.unconditionallyProcess(thisPCTemplate, "CHOOSE", "LANGAUTO|"
-					+ aString);
+				if (sel.length > 0)
+				{
+					aString = EditUtil.delimitArray(sel, '|');
+					context.unconditionallyProcess(thisPCTemplate, "CHOOSE", "LANGAUTO|"
+						+ aString);
+				}
 
 				//
 				// Save feats
 				//
 				thisPCTemplate.removeListFor(ListKey.FEAT_TOKEN_LIST);
 				sel = pnlFeats.getSelectedList();
-				aString = EditUtil.delimitArray(sel, '|');
-				context.unconditionallyProcess(thisPCTemplate, "FEAT", aString);
+				if (sel.length > 0)
+				{
+					aString = EditUtil.delimitArray(sel, '|');
+					context.unconditionallyProcess(thisPCTemplate, "FEAT", aString);
+				}
 
 				//
 				// Save bonus languages
 				//
 				thisPCTemplate.removeAllFromList(Language.STARTING_LIST);
 				sel = pnlBonusLang.getSelectedList();
-				aString = EditUtil.delimitArray(sel, ',');
-				context.unconditionallyProcess(thisPCTemplate, "LANGBONUS", aString);
+				if (sel.length > 0)
+				{
+					aString = EditUtil.delimitArray(sel, ',');
+					context.unconditionallyProcess(thisPCTemplate, "LANGBONUS", aString);
+				}
 
 				//
 				// Save level and hit dice abilities
@@ -1030,8 +1045,11 @@ public final class EditorMainForm extends JDialog
 				}
 				selList.append((String) sel[i]);
 			}
-			context.unconditionallyProcess(thisPObject, "AUTO:WEAPONPROF",
-					selList.toString());
+			if (selList.length() > 0)
+			{
+				context.unconditionallyProcess(thisPObject, "AUTO:WEAPONPROF",
+						selList.toString());
+			}
 
 			sel = pnlWeapons.getSelectedList2();
 
@@ -1041,7 +1059,7 @@ public final class EditorMainForm extends JDialog
 				  || editType == EditorConstants.EDIT_RACE)
 				{
 					thisPObject.removeAllFromList(WeaponProf.STARTING_LIST);
-					context.unconditionallyProcess(thisPObject, "LANGBONUS",
+					context.unconditionallyProcess(thisPObject, "WEAPONBONUS",
 							EditUtil.delimitArray(sel, ','));
 				}
 			}
