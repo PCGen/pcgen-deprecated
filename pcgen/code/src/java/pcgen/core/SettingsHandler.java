1cb8c6274c2b75d66eca32da6ffefbdf8ce0bd59
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-07-14 22:34:18 +0000

    By default do not load sources with PC
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/5.16.x@12569 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- pcgen/code/src/java/pcgen/core/SettingsHandler.java
+++ pcgen/code/src/java/pcgen/core/SettingsHandler.java
@@ -1230,7 +1230,7 @@ public final class SettingsHandler
 		setInvalidToHitText(getPCGenOption("invalidToHitText", PropertyFactory.getString("SettingsHandler.114")));  //$NON-NLS-1$//$NON-NLS-2$
 		setLastTipShown(getPCGenOption("lastTipOfTheDayTipShown", -1)); //$NON-NLS-1$
 		setLoadCampaignsAtStart(getPCGenOption("loadCampaignsAtStart", false)); //$NON-NLS-1$
-		setLoadCampaignsWithPC(getPCGenOption("loadCampaignsWithPC", true)); //$NON-NLS-1$
+		setLoadCampaignsWithPC(getPCGenOption("loadCampaignsWithPC", false)); //$NON-NLS-1$
 		setLookAndFeel(getPCGenOption("looknFeel", 1)); //$NON-NLS-1$
 		setMaxPotionSpellLevel(getPCGenOption("maxPotionSpellLevel", 3)); //$NON-NLS-1$
 		setMaxWandSpellLevel(getPCGenOption("maxWandSpellLevel", 4)); //$NON-NLS-1$
