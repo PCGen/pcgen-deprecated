7222a4ee38838f4fe1847e7aad426435c699bbbb
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-10-19 21:43:30 +0000

    Fix bug: Innate Spell LONG Range displaying incorrectly
    Issue#: CODE-246
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/5.16.x@13507 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- pcgen/code/src/java/pcgen/core/PlayerCharacter.java
+++ pcgen/code/src/java/pcgen/core/PlayerCharacter.java
@@ -6847,10 +6847,10 @@ public final class PlayerCharacter extends Observable implements Cloneable,
 			if (si != null)
 			{
 				metaFeats = si.getFeatList();
-				rangeInFeet =
-						getVariableValue(aSpell, aString, aSpellClass)
-							.intValue();
 			}
+			rangeInFeet =
+				getVariableValue(aSpell, aString, aSpellClass)
+					.intValue();
 			if ((metaFeats != null) && !metaFeats.isEmpty())
 			{
 				for (Ability feat : metaFeats)
