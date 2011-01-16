cba07042a3cc8d0f29ba641a8f85c7fc89824311
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2011-01-16 04:15:10 +0000

    Correct link to doco page
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@14370 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- website/04_support.php
+++ website/04_support.php
@@ -10,13 +10,13 @@ require_once('include/header.php.inc');
 
         <h2>Online Documentation</h2>
 
-        <p>Before asking someone for help, please read <a href="autobuilds/pcgen-docs/" title="PCGen Documentation">the documentation</a> (this documentation is also included in the full PCGen download archive). There is a very strong chance that your question has already been asked and answered, and that the answer is waiting for you in the PCGen docs. Imagine the pride you will feel at being able to solve your own problem.</p>
+        <p>Before asking someone for help, please read <a href="http://www.pcgen-test.org/autobuilds/pcgen-docs/index.html" title="PCGen Documentation">the documentation</a> (this documentation is also included in the full PCGen download archive). There is a very strong chance that your question has already been asked and answered, and that the answer is waiting for you in the PCGen docs. Imagine the pride you will feel at being able to solve your own problem.</p>
 
         <h2>Technical Support Overview</h2>
 
         <p>Technical and user-to-user support for PCGen are made possible with tools provided by <a href="https://sourceforge.net/" title="SourceForge.net">SourceForge</a> and <a href="http://groups.yahoo.com/" title="YahooGroups">Yahoo</a>.  Actual items that are being tracked for development such as bugs and feature requests are handled by <a href="http://jira.pcgen.org">JIRA</a>.  You can get support in the following ways:</p>
         <ol>
-            <li>For almost every issue that needs attention you can go to one of the approriate PCGen groups <a href="http://groups.yahoo.com/search?query=pcgen&amp;submit=Search" title="You must log into YahooGroups in order to subscribe to the email lists.">email lists</a>. It is important to send your question to the appropriate mailing list. For example, an issue pertaining to a list file design problem should be directed to the PCGenListFileHelp list, not the PCGenView list.</li>
+            <li>For almost every issue that needs attention you can go to one of the appropriate PCGen groups <a href="http://groups.yahoo.com/search?query=pcgen&amp;submit=Search" title="You must log into YahooGroups in order to subscribe to the email lists.">email lists</a>. It is important to send your question to the appropriate mailing list. For example, an issue pertaining to a list file design problem should be directed to the PCGenListFileHelp list, not the PCGenView list.</li>
             <li>Alternatively you can record your issue in the <a href="http://jira.pcgen.org" title="You must log into JIRA in order to submit or monitor issues.">JIRA</a>.</li>
         </ol>
 
