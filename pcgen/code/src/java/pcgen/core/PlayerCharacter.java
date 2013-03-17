e1e11a076f767104a9e231a0255b611433791608
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2013-03-17 04:39:52 +0000

    Fix bug: Bardic Abilities showing 0 in output when DEFINE and BONUS:VAR are set correctly in the granted ability Issue#: CODE-2086
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/branches/6.00.x@19616 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- pcgen/code/src/java/pcgen/core/PlayerCharacter.java
+++ pcgen/code/src/java/pcgen/core/PlayerCharacter.java
@@ -1005,6 +1005,10 @@ public class PlayerCharacter extends Observable implements Cloneable, VariableCo
 
 	/**
 	 * Sets the character changed since last save.
+	 * NB: This is not a 'safe' call - its use should be considered carefully and in 
+	 * particular it should not be called from a method used as part of PlayerCharacter 
+	 * cloning as this can mean conditional abilities get dropped when they are actually 
+	 * qualified for, just not at that point in the clone.
 	 * 
 	 * @param dirtyState the new "dirty" value (may be false to indicate no change)
 	 */
@@ -3022,7 +3026,7 @@ public class PlayerCharacter extends Observable implements Cloneable, VariableCo
 	public void addEquipSet(final EquipSet set)
 	{
 		equipSetFacet.add(id, set);
-		setDirty(true);
+		//setDirty(true);
 	}
 
 	/**
@@ -5542,7 +5546,7 @@ public class PlayerCharacter extends Observable implements Cloneable, VariableCo
 		if (!spellBookFacet.containsBookNamed(id, book.getName()))
 		{
 			spellBookFacet.add(id, book);
-			setDirty(true);
+			//setDirty(true);
 			return true;
 		}
 		return false;
@@ -8681,6 +8685,7 @@ public class PlayerCharacter extends Observable implements Cloneable, VariableCo
 		// be able to reset them. Need to call new PlayerCharacter()
 		// aClone = (PlayerCharacter)super.clone();
 		aClone = new PlayerCharacter(true, campaignFacet.getSet(id));
+		aClone.variableProcessor = new VariableProcessorPC(aClone);
 		try
 		{
 			aClone.assocSupt = assocSupt.clone();
@@ -8764,8 +8769,6 @@ public class PlayerCharacter extends Observable implements Cloneable, VariableCo
 		// order in which the skills will be output.
 		aClone.skillsOutputOrder = skillsOutputOrder;
 		aClone.spellLevelTemp = spellLevelTemp;
-		// Is this OK?
-		aClone.variableProcessor = new VariableProcessorPC(aClone);
 		aClone.pointBuyPoints = pointBuyPoints;
 
 		aClone.setDirty(true);
